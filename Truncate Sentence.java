89acfc2db3539c02ed2c4f099e5f2478c8948dae
Rohan <rohansethi1999430@gmail.com>
2021-05-31 20:59:54 +0530

    Truncate Sentence.java

---
+++ Truncate Sentence.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String truncateSentence(String s, int k) {
+        StringBuilder sb = new StringBuilder();
+        String[] words = s.split(" ");
+
+        for (int i = 0; i < k; i++)
+            sb.append(words[i]).append(" ");
+
+        return sb.toString().trim();
+    }
+}
