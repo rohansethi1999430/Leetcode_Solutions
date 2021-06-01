ad855b7136052ab1b7da33550ad14bb1a3d99241
Rohan <rohansethi1999430@gmail.com>
2021-06-01 23:27:57 +0530

    Determine if String Halves Are Alike.java

---
+++  Determine if String Halves Are Alike.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean halvesAreAlike(String s) {
+        int half = s.length() / 2;
+        int count1 = 0;
+        int count2 = 0;
+        // String first_half=s.substring(0,half);
+        // String second_half=s.substring(half,s.length());
+        s = s.toLowerCase();
+        for (int i = 0; i < half; i++) {
+            if (s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o'
+                    || s.charAt(i) == 'u') {
+                count1++;
+            }
+        }
+        for (int i = half; i < s.length(); i++) {
+            if (s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o'
+                    || s.charAt(i) == 'u') {
+                count2++;
+            }
+        }
+        if (count1 == count2) {
+            return true;
+        } else
+            return false;
+    }
+
+}
