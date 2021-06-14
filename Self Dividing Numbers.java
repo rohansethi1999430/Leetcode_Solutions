d1d0d5afb51787bd798cc4568aea436d50ee9106
Rohan <rohansethi1999430@gmail.com>
2021-06-14 21:54:02 +0530

    Self Dividing Numbers.java

---
+++ Self Dividing Numbers.java
@@ -0,0 +1,8 @@
+public boolean selfDividing(int n) {
+        int x = n;
+        while (x > 0) {
+            int d = x % 10;
+            x /= 10;
+            if (d == 0 || (n % d) > 0) return false;
+        }
+        return true;
